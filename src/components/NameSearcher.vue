2ba8c6a40d5d88ea1ef25333a1a1e26740b09808
Nestor <zozomezo@abv.bg>
2023-02-21 17:43:07 +0200

    Input field added

--- src/components/NameSearcher.vue
+++ src/components/NameSearcher.vue
@@ -1,21 +1,54 @@
 <template>
-  <div class="container"></div>
-  <name-search-response></name-search-response>
+    <div class="container">
+        <el-form :model="form" label-width="120px">
+            <el-form-item label="Activity name">
+                <el-input v-model="form.name" placeholder="Please input" />
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" @click="handleSubmit"
+                    >Check</el-button
+                >
+                <el-button>Cancel</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+    <name-search-response></name-search-response>
 </template>
 
 <script>
 import NameSearchResponse from '@/components/NameSearchResponse.vue'
 
 export default {
-  name: 'NameSearcher',
-  components: {
-    NameSearchResponse,
-  },
+    name: 'NameSearcher',
+    components: {
+        NameSearchResponse,
+    },
+    data() {
+        return {
+            form: {
+                name: '',
+            },
+        }
+    },
+    methods: {
+        handleSubmit() {
+            // this.selected = event.target.value
+            // this.editStudent.favoriteInstructorId = event.target.value
+            // this.validateInstructors()
+            console.log('Submitted')
+        },
+    },
 }
+import { reactive } from 'vue'
+
+const form = reactive({
+    name: '',
+})
 </script>
 
 <style scoped lang="scss">
 .container {
-  width: 50%;
+    width: 50%;
 }
 </style>
